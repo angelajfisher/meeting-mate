12bf790d31be13d7427cd2505763c3be603482e3
Angela Fisher <ajf-noreply@phantom-mail.io>
2024-08-09 18:31:45 -0700

    🏷️ Add types to handle Zoom event data

---
+++ internal/data/data.go
@@ -0,0 +1,15 @@
+package data
+
+type EventData struct {
+	EventType       string
+	MeetingName     string
+	ParticipantName string
+}
+
+const (
+	EndpointValidation = "endpoint.url_validation"
+	MeetingStart       = "meeting.started"
+	MeetingEnd         = "meeting.ended"
+	ParticipantJoin    = "meeting.participant_joined"
+	ParticipantLeave   = "meeting.participant_left"
+)
--- internal/server/webhooks.go
+++ internal/server/webhooks.go
@@ -8,13 +8,11 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/angelajfisher/zoom-bot/internal/data"
 )
 
 var Secret string
-
-const (
-	EndpointValidation = "endpoint.url_validation"
-)
 
 type ZoomData struct {
 	Payload interface{} `json:"payload"`
@@ -25,6 +23,37 @@ type ZoomData struct {
 type URLValidation struct {
 	PlainToken     string `json:"plainToken"`
 	EncryptedToken string `json:"encryptedToken,omitempty"`
+}
+
+type Meeting struct {
+	Duration    uint32      `json:"duration"`
+	StartTime   string      `json:"start_time,omitempty"`
+	EndTime     string      `json:"end_time,omitempty"`
+	Timezone    string      `json:"timezone"`
+	Topic       string      `json:"topic"`
+	ID          string      `json:"id"`
+	Type        uint8       `json:"type"`
+	UUID        string      `json:"uuid"`
+	HostID      string      `json:"host_id"`
+	Participant Participant `json:"participant,omitempty"`
+}
+
+type Participant struct {
+	UserID            string `json:"user_id"`
+	UserName          string `json:"user_name"`
+	ParticipantUserID string `json:"participant_user_id"`
+	ID                string `json:"id"`
+	JoinTime          string `json:"join_time,omitempty"`
+	LeaveTime         string `json:"leave_time,omitempty"`
+	Email             string `json:"email"`
+	ParticipantUUID   string `json:"participant_uuid"`
+	LeaveReason       string `json:"leave_reason,omitempty"`
+}
+
+// Wrapper to handle extracting nested data from Zoom webhook.
+// Use with `json.Unmarshal(payload, &ObjectWrapper{&dataStruct})`
+type ObjectWrapper struct {
+	Object interface{} `json:"object"`
 }
 
 func handleWebhooks(w http.ResponseWriter, r *http.Request) {
@@ -39,27 +68,26 @@ func handleWebhooks(w http.ResponseWriter, r *http.Request) {
 	log.Println(string(reqBody))
 
 	var payload json.RawMessage
-	data := ZoomData{Payload: &payload}
-	err = json.Unmarshal(reqBody, &data)
+	eventData := ZoomData{Payload: &payload}
+	err = json.Unmarshal(reqBody, &eventData)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	switch data.Event {
-	case EndpointValidation:
-		var eventData URLValidation
-		err = json.Unmarshal([]byte(payload), &eventData)
+	if eventData.Event == data.EndpointValidation {
+		var payloadData URLValidation
+		err = json.Unmarshal(payload, &payloadData)
 		if err != nil {
 			log.Println(err)
 		}
 
 		hasher := hmac.New(sha256.New, []byte(Secret))
-		hasher.Write([]byte(eventData.PlainToken))
+		hasher.Write([]byte(payloadData.PlainToken))
 
-		eventData.EncryptedToken = hex.EncodeToString(hasher.Sum(nil))
+		payloadData.EncryptedToken = hex.EncodeToString(hasher.Sum(nil))
 
-		retBody, err := json.Marshal(eventData)
+		retBody, err := json.Marshal(payloadData)
 		if err != nil {
 			log.Println(err)
 			return
@@ -71,6 +99,26 @@ func handleWebhooks(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Println(err)
 			return
+		}
+	} else {
+		var payloadData Meeting
+		err = json.Unmarshal(payload, &ObjectWrapper{&payloadData})
+		if err != nil {
+			log.Println(err)
+		}
+
+		switch eventData.Event {
+		case data.MeetingStart:
+			log.Printf("Meeting '%v' started at %v\n", payloadData.Topic, payloadData.StartTime)
+
+		case data.MeetingEnd:
+			log.Printf("Meeting '%v' ended at %v\n", payloadData.Topic, payloadData.EndTime)
+
+		case data.ParticipantJoin:
+			log.Printf("%v joined '%v' at %v\n", payloadData.Participant.UserName, payloadData.Topic, payloadData.Participant.JoinTime)
+
+		case data.ParticipantLeave:
+			log.Printf("%v left '%v' at %v\n", payloadData.Participant.UserName, payloadData.Topic, payloadData.Participant.LeaveTime)
 		}
 	}
 
